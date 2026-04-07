ac36e330648fe71d28f859a1627d906ec0352d18
Aditya Kumar <adityaforwork101@gmail.com>
2026-04-07 14:31:04 +0530

    theme context

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeDataContext } from '../context/ThemeContext';
 
 function Logo() {
+
+  const { theme, setTheme } = useContext(ThemeDataContext);
+
   return (
-    <div>
+    <div className='relative'>
       <h2 className='text-center font-bold text-3xl md:text-6xl text-cyan-200 p-2 text-shadow-lg text-shadow-amber-800'>Tic Tac Task</h2>
+      <button className='absolute top-2.5 right-2 md:top-7 md:right-10 bg-cyan-700 text-white p-1 md:p-2 rounded-full hover:bg-cyan-600' onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+        {theme === 'light' ? 'Dark' : 'Light'}
+      </button>
     </div>
   )
 }
---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState } from 'react'
+
+export const ThemeDataContext = createContext();
+
+function ThemeContext(props) {
+
+    const [theme, setTheme] = useState('light');
+
+  return (
+    <div>
+      <ThemeDataContext.Provider value={{ theme, setTheme }}>
+        {props.children}
+      </ThemeDataContext.Provider>
+    </div>
+  )
+}
+
+export default ThemeContext
